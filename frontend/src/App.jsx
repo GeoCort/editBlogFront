9ac6e1db5f28f3f911b593f39f1a79aa7770d3bc
George Cortes <93002810+GeoCort@users.noreply.github.com>
2024-11-08 00:54:56 -0800

    added auth connected to falconest api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header.jsx'
 import Sidebar from './components/main/Sidebar.jsx'
 import Body from './components/main/Body.jsx'
+import Login from './components/auth/Login.jsx'
 function App() {
-
+  const [auth, setAuth] = useState(false);
+  useEffect( ()=>{
+    if(localStorage.getItem("token")){
+      setAuth(true)
+    }
+  })
   return (
-   <>
-      <Header/>
-      <Body/>
-      <Sidebar/>
+   <> 
+      {auth == true? <Body/> : <Login/>}
     </>
   )
 }
---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import axios from "axios"
+export default function Login(props){
+    let defaultForm = {
+        username: "",
+        password: ""
+    }
+    const [loginForm, setLoginForm] = useState(defaultForm);
+    // handle input change using state
+    const handleChange = (e)=>{
+        let {name, value} = e.target
+        setLoginForm((preForm)=> ({...preForm, [name]:value}))
+    }
+    // handle Login event
+    const loginAuth = async (e)=>{
+        e.preventDefault()
+        console.log("clicked and working")
+        let response =  await axios.post(`https://backendblogapi-b563b8bcb606.herokuapp.com/api/login`, loginForm)
+        if(response.status == 200){
+            localStorage.setItem("token", response.data.token)
+            localStorage.setItem("user", response.data.user.id)
+        }
+        
+    }
+    return(
+        <section>
+            <form 
+            onSubmit={loginAuth}
+            >   
+                <h2>Welcome Back. </h2>
+                <div>
+                <label htmlFor="username-input">Username</label>
+                <input placeholder="username" type="text" name="username" id="username-input" required min={4} max={35} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="password-input">Password</label>
+                    <input type="password" placeholder="password" name="password" min={4} max={50} required onChange={handleChange}/>
+                </div>
+                <button type="submit"  >Login</button>
+            </form>
+        </section>
+    )
+}
