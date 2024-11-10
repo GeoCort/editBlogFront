23f26abac7b5dad79bbd5fda92184a3c0a8da392
George Cortes <93002810+GeoCort@users.noreply.github.com>
2024-11-10 13:56:45 -0800

    included API

--- frontend/src/components/main/Edit.jsx
+++ frontend/src/components/main/Edit.jsx
@@ -9,33 +9,46 @@ export default function Edit(){
     return (
         <>
         <section className="edit">
-            <p className="edit-header">
-            Lorem ipsum, dolor sit 
-            </p>
+            <h1 className="edit-header">
+            Create a New Post
+            </h1>
             <div className="edit-section">
-                <input type="text"/> 
+                <div>
+                    <label htmlFor="titleInput">Title</label>
+                    <input type="text" placeholder="Title" id="titleInput" required/> 
+                </div>
+                <div>
+                    <label htmlFor="subTitleInput">Subtitle</label>
+                    <input type="text" placeholder="Subtitle" id="subTitleInput"/> 
+                </div>
             </div>
-      <Editor
-        apiKey='your-api-key'
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-          ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <button onClick={log}>Log editor content</button>
-   
+            <Editor
+      apiKey={process.env.VITE_TINYMC_API_KEY}
+      init={{
+        plugins: [
+          // Core editing features
+          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+          // Your account includes a free trial of TinyMCE premium features
+          // Try the most popular premium features until Nov 24, 2024:
+          'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+          // Early access to document converters
+          'importword', 'exportword', 'exportpdf'
+        ],
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+        exportpdf_converter_options: { 'format': 'Letter', 'margin_top': '1in', 'margin_right': '1in', 'margin_bottom': '1in', 'margin_left': '1in' },
+        exportword_converter_options: { 'document': { 'size': 'Letter' } },
+        importword_converter_options: { 'formatting': { 'styles': 'inline', 'resets': 'inline',	'defaults': 'inline', } },
+      }}
+      initialValue="Welcome to TinyMCE!"
+    />
+        <button className="endBtn">Create Post</button>
         </section>
         </>
     )
